c7f59a5f21ab348149f19e9d70899e19a3dfd11c
rithvik <innovation6178@gmail.com>
2023-04-27 10:34:20 +0530

    made index.js as entry point of project

---
+++ index.js
@@ -0,0 +1,9 @@
+const {conf} = require('./HTTP/initConfig')
+const {initServer, initDB} = require('./HTTP/initServices')
+const {initHandler} = require("./HTTP/initHandler")
+
+
+const config = conf;
+const app = initServer(config)
+initDB(config)
+initHandler(app)
