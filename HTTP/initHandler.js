be22b1a22000179c55a6a962d456f70db20bfcbf
rithvik <innovation6178@gmail.com>
2023-04-28 10:18:37 +0530

    added retailers auth handler and logic

--- HTTP/initHandler.js
+++ HTTP/initHandler.js
@@ -1,6 +1,21 @@
 function initHandler(app){
     app.get('/health',function(req,res,next){
           res.send("Health OK")
+    });
+    app.post('/retailersAuth', (req, res) => {
+        const { email, password } = req.body;
+        console.log(req.body);
+        if (!email || !password) return res.json({ msg: "no password or email" })
+        else if (email === "abcd" && password === "abcd")
+            res.json({ token: TOKEN })
+    })
+    app.post('/retailersDashboard', (req, res) => {
+        const { token } = req.body;
+        console.log(req.body)
+        if (token === TOKEN)
+            res.json({ in: empData, out: empData })
+        else
+            return res.json({ msg: "token missing" })
     })
 }
 
