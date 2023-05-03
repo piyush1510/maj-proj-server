0749f6869c6d9eb23e1a0d6e0e9dcb800e4e2372
rithvik <innovation6178@gmail.com>
2023-05-03 11:51:29 +0530

    migration tables added

--- migration_up.sql
+++ migration_up.sql
@@ -3,4 +3,25 @@ create table if not exists products(
     product_hash_id varchar(100)
     product_name  varchar(100)
     packaged_date Date
-)
+    primary key product_id
+);
+
+create table if not exists quality(
+    product_id int,
+    temperature int,
+    humidity int,
+    gas int,
+    taken_at Date,
+    primary key (product_id,taken_at)
+);
+
+create table if not exists quantity(
+    product_id int,
+    product_stop varchar(50),
+    weight_stop int,
+    taken_at Date,
+    primary key (product_id, taken_at)
+);
+
+
+
