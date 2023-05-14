e559ea301f5c304d467d93ab351b61ca6fd4970a
rithvik <innovation6178@gmail.com>
2023-05-14 19:19:30 +0530

    added api logic

--- HTTP/initHandler.js
+++ HTTP/initHandler.js
@@ -19,20 +19,41 @@ for (let i = 0; i < 15; i++) {
 }
 
 
+const customerDetails = require('../Services/customerDetails')
 
-function initHandler(app){
+
+function initHandler(app,config){
     app.get('/health',function(req,res,next){
           res.send("Health OK")
     });
 
     app.post('/productDetails', (req, res) => {
-        const { productId } = req.body;
-        res.json({
-            temp: tempFakeData,
-            mq: mqFakeData,
-            p: perishedPercentage
+        const { product_name, device_mac_id } = req.body;
+        config.connections.db.execute(`insert into products 
+        (product_name,device_mac_id) 
+        values (${product_name},${device_mac_id});`,(error,results,fields)=>{
+            if(error){
+               console.log(error)
+            }
+            else{
+                return res.json({status:"sucessfully inserted"});
+            }
         })
     });
+
+    app.get('/productDetails',(req,res)=>{
+        config.connections.db.execute(
+            `select * from products;`,
+            (err,results,fields)=>{
+                if(err){
+                   console.log(err)
+                }
+                else{
+                  return res.json({products:results})
+                }
+            }
+        )
+    })
 
     app.post('/retailersAuth', (req, res) => {
         const { email, password } = req.body;
@@ -49,6 +70,91 @@ function initHandler(app){
         else
             return res.json({ msg: "token missing" })
     })
+
+    app.get('/customerDetails', (req,res)=>{
+         config.connections.db.execute(
+            `select * from users ;`,(err,result,fields)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                   console.log(result);
+                   return res.json({details: result});
+                }
+            }
+        );
+    })
+
+    app.post('/customerDetails', (req,res)=>{
+        config.connections.db.execute(
+            `insert into users ( user_name, designation) 
+            values ("${req.body.user_name}","${req.body.designation}"); `,(err,result,fields)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                   console.log(result);
+                   return res.json({status:"sucessfully inserted"});
+                }
+            }
+        );
+    })
+
+    app.get('/quailityDetails',(req,res)=>{
+        config.connections.db.execute(
+            `select * from quality where
+             product_id = ${req.body.product_id};`,(err,results,fields)=>{
+                if(err){
+                  console.log(err)
+                }
+                else{
+                    return res.json({quality_details:results})
+                }
+             }
+        )
+    })
+
+    app.post('qualityDetails',(req,res)=>{
+        config.connections.db.execute(
+            `insert into quality ( product_id, temperature, humidity, gas) 
+            values (${req.body.product_id},${req.body.temperature}, ${req.body.humidity}, ${req.body.gas}); `,(err,result,fields)=>{
+                if(err){
+                }
+                else{
+                   return res.json({status:"sucessfully inserted"});
+                }
+            }
+        );
+    })
+
+    app.get('/quantityDetails',(req,res)=>{
+        config.connections.db.execute(
+            `select * from aunatity where
+             product_id = ${req.body.product_id};`,(err,results,fields)=>{
+                if(err){
+                  console.log(err)
+                }
+                else{
+                    return res.json({quantity_details:results})
+                }
+             }
+        )
+    })
+
+    app.post('quantityDetails',(req,res)=>{
+        config.connections.db.execute(
+            `insert into quantity ( product_id, product_stop, weight_stop) 
+            values (${req.body.product_id},${req.body.temperature}, ${req.body.humidity}, ${req.body.gas}); `,(err,result,fields)=>{
+                if(err){
+                    console.log(err)
+                }
+                else{
+                   return res.json({status:"sucessfully inserted"});
+                }
+            }
+        );
+    })
+
 }
 
 module.exports = {initHandler}
